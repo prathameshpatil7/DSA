e0477622020fb104113906d62a2fa3f957c41d60
Prathamesh Patil <75564009+prathameshpatil7@users.noreply.github.com>
2025-02-01 23:54:43 +0530

    Time: 2 ms (40.95%) | Memory: 50.4 MB (11.48%) - LeetSync

--- 392-is-subsequence/is-subsequence.js
+++ 392-is-subsequence/is-subsequence.js
@@ -4,12 +4,29 @@
  * @return {boolean}
  */
 var isSubsequence = function(s, t) {
-    let i = 0, j = 0;
-    while (i < s.length && j < t.length) {
-        if (s[i] === t[j]) {
-            i++;
+    // let i = 0, j = 0;
+    // while (i < s.length && j < t.length) {
+    //     if (s[i] === t[j]) {
+    //         i++;
+    //     }
+    //     j++;
+    // }
+    // return i === s.length;
+     let n = s.length;
+    let m = t.length;
+    let k = 0;
+    let str = "";
+    for(let i = 0; i<n; i++)
+    {
+        for(let j = k; j<m; j++)
+        {
+            if(s[i] == t[j])
+            {
+                k = j+1;
+                str += s[i];
+                break;
+            }
         }
-        j++;
     }
-    return i === s.length;
+    return s === str;
 };
