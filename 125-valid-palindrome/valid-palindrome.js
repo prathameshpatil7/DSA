ac9564862aa725488f696e24a5f0e09081bbe39a
Prathamesh Patil <75564009+prathameshpatil7@users.noreply.github.com>
2025-02-04 23:59:04 +0530

    Time: 3 ms (85.78%) | Memory: 51.1 MB (87.56%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -3,20 +3,14 @@
  * @return {boolean}
  */
 var isPalindrome = function (s) {
+    let str = s.toLowerCase().replace(/[^a-z0-9]/g, "");
     let start = 0;
-    let n = s.length;
+    let n = str.length;
     let end = n - 1;
     let result = true;
     while (start <= end) {
-        if (!isAlphaNumeric(s[start])) {
-            start++;
-            continue;
-        }
-        if (!isAlphaNumeric(s[end])) {
-            end--;
-            continue;
-        }
-        if (s[start].toLowerCase() === s[end].toLowerCase()) {
+
+        if (str[start] === str[end]) {
             start++;
             end--;
         } else {
