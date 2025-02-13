9717c5c3655f73639893d880f8257b862b365863
Prathamesh Patil <75564009+prathameshpatil7@users.noreply.github.com>
2025-02-13 23:41:24 +0530

    Time: 8 ms (65.24%) | Memory: 55.2 MB (64.20%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.js
+++ 6-zigzag-conversion/zigzag-conversion.js
@@ -5,20 +5,38 @@
  */
 var convert = function (s, numRows) {
     // 5 3 1
-    if (numRows === 1) {
-        return s;
+    // if (numRows === 1) {
+    //     return s;
+    // }
+
+    // let result = "";
+    // let n= s.length;
+    // let cycLen = 2 * numRows - 2;
+    // for (let i = 0; i < numRows; i++) {
+    //     for (let j = 0; j + i < n; j += cycLen) {
+    //         result += s[j + i];
+    //         if (i !== 0 && i !== numRows - 1 && j + cycLen - i < n) {
+    //             result += s[j + cycLen - i];
+    //         }
+    //     }
+    // }
+    // return result;
+
+    if (numRows === 1) return s; // No zigzag needed
+
+    let rows = new Array(Math.min(numRows, s.length)).fill("").map(() => ""); 
+    let currRow = 0;
+    let goingDown = false;
+
+    // Traverse the string
+    for (let c of s) {
+        rows[currRow] += c;
+        if (currRow === 0 || currRow === numRows - 1) {
+            goingDown = !goingDown; // Change direction
+        }
+        currRow += goingDown ? 1 : -1;
     }
 
-    let result = "";
-    let n= s.length;
-    let cycLen = 2 * numRows - 2;
-    for (let i = 0; i < numRows; i++) {
-        for (let j = 0; j + i < n; j += cycLen) {
-            result += s[j + i];
-            if (i !== 0 && i !== numRows - 1 && j + cycLen - i < n) {
-                result += s[j + cycLen - i];
-            }
-        }
-    }
-    return result;
+    // Concatenate rows to form final output
+    return rows.join("");
 };
